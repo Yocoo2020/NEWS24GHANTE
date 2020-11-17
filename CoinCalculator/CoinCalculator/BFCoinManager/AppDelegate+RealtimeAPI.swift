89d3f1e63bcad8ad2466874a7a9d984287e424e6
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2020-11-17 12:15:23 +0000

    More readme edits

--- CoinCalculator/CoinCalculator/BFCoinManager/AppDelegate+RealtimeAPI.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/AppDelegate+RealtimeAPI.swift
@@ -10,3 +10,8 @@ import UIKit
 import PubNub
 
 extension AppDelegate : PNObjectEventListener {
+    // Handle new message from one of channels on which client has been subscribed.
+    func client(_ client: PubNub, didReceiveMessage message: PNMessageResult) {
+
+        // Handle new message stored in message.data.message
+        if message.data.channel != mess
