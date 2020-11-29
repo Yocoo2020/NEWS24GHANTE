dd853967a3ddc068199648f6c6153c1b51714f9d
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2020-11-29 11:49:25 +0000

    sdi

--- CoinCalculator/CoinCalculator/BFCoinManager/BFCoinManager.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/BFCoinManager.swift
@@ -1,2 +1,13 @@
 //
-//  BFCoinManager.
+//  BFCoinManager.swift
+//  BFCoinManager
+//
+//  Created by jaeeun on 2018/01/30.
+//  Copyright © 2018年 leejaeeun. All rights reserved.
+//
+
+import Foundation
+import PubNub
+
+protocol BFCoinManagerDataChanged : AnyObject {
+    func coinDataDidLoad(_ context:
