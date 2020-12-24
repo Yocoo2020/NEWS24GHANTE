6cc8ceee3bf88cf693a4233940f45dcff7493b6f
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2020-12-24 13:10:34 +0000

    usb: gadget: f_mbim: Change %p to %pK in debug messages

--- CoinCalculator/CoinCalculator/BFCoinManager/Extension/Date+Helper.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/Extension/Date+Helper.swift
@@ -3,4 +3,15 @@
 //  BFCoinManager
 //
 //  Created by jaeeun on 2018/02/03.
-//  Copyright © 2018年 leejaeeun. All right
+//  Copyright © 2018年 leejaeeun. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    func dateString()-> String {
+        //"2018-02-03T02:28:43.247"
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateForma
