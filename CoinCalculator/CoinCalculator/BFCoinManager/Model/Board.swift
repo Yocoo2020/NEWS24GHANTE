54b56e84901134adcd319de6878c350ffb80ccb2
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2020-12-27 18:41:46 +0000

    Coverity fix.

--- CoinCalculator/CoinCalculator/BFCoinManager/Model/Board.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/Model/Board.swift
@@ -3,4 +3,17 @@
 //  BFCoinManager
 //
 //  Created by jaeeun on 2018/01/31.
-//  Copyright © 2018年 leejaeeun. All rights reser
+//  Copyright © 2018年 leejaeeun. All rights reserved.
+//
+
+import Foundation
+
+/*
+ {"mid_price": 33320,
+ "bids": [{"price": 30000,
+         "size": 0.1},
+         {"price": 25570,
+         "size": 3}],
+ "asks": [{"price": 36640,
+         "size": 5},
+         {"price": 3
