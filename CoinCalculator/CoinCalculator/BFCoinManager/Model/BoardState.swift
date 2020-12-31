1839dcc52cf2fddc2ca7de6ffc4800979250739a
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2020-12-31 10:15:11 +0000

    Create travis.yml

--- CoinCalculator/CoinCalculator/BFCoinManager/Model/BoardState.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/Model/BoardState.swift
@@ -12,4 +12,11 @@ enum HealthType: String {
     case normal      = "NORMAL"      //取引所は稼動しています。
     case busy        = "BUSY"        //取引所に負荷がかかっている状態です。
     case veryBusy    = "VERY BUSY"   //取引所の負荷が大きい状態です。
-    case superBusy   = "SUPER BUSY"  //負荷が非
+    case superBusy   = "SUPER BUSY"  //負荷が非常に大きい状態です。発注は失敗するか、遅れて処理される可能性があります。
+    case noOrder     = "NO ORDER"    //発注が受付できない状態です。
+    case stop        = "STOP"        //取引所は停止しています。発注は受付されません。
+}
+
+enum StateType: String {
+    case running        = "RUNNING"         //通常稼働中
+  
