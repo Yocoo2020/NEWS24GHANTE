dad73ca7183cdca225b5396c54b62c582537abd3
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2021-01-02 05:38:10 +0000

    update tests for latest AVA version

--- CoinCalculator/CoinCalculator/BFCoinManager/Model/Execution.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/Model/Execution.swift
@@ -1,2 +1,16 @@
 //
-//  Execution.
+//  Execution.swift
+//  BFCoinManager
+//
+//  Created by jaeeun on 2018/01/31.
+//  Copyright © 2018年 leejaeeun. All rights reserved.
+//
+
+import Foundation
+
+enum SideType : String {
+    case buy    = "BUY"
+    case sell   = "SELL"
+}
+/*
+    "id"
