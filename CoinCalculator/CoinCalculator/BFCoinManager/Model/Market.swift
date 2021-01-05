d07d96cc1a977ecf48288aed13f422c64196484c
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2021-01-05 16:46:55 +0000

    ACPICA: Utilities: split IO address types from data type models.

--- CoinCalculator/CoinCalculator/BFCoinManager/Model/Market.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/Model/Market.swift
@@ -3,4 +3,14 @@
 //  BFCoinManager
 //
 //  Created by jaeeun on 2018/01/31.
-//  C
+//  Copyright © 2018年 leejaeeun. All rights reserved.
+//
+
+import Foundation
+
+/*
+    [{ "product_code": "BTC_JPY" },
+    { "product_code": "FX_BTC_JPY" },
+    { "product_code": "ETH_BTC" },
+    {
+    "product_code": "BTCJPY28APR20
