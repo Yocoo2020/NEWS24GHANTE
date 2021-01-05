8c97f6bd2a885b22aee44f21b8e53b64545be57b
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2021-01-05 12:33:41 +0000

    Layout adjustments

--- CoinCalculator/CoinCalculator/BFCoinManager/Model/Health.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/Model/Health.swift
@@ -1,2 +1,15 @@
 //
-//  Health.swif
+//  Health.swift
+//  BFCoinManager
+//
+//  Created by jaeeun on 2018/01/31.
+//  Copyright © 2018年 leejaeeun. All rights reserved.
+//
+
+import Foundation
+
+struct Health {
+    var status: HealthType?
+    
+    init(dictionary: Dictionary<String, Any>) {
+     
