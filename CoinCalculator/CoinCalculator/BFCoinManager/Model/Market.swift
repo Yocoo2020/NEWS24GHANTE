581b4b27da456ba7badace5ac5379a94f05efa05
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2021-01-05 21:57:17 +0000

    Run update-remote.sh on adoptopenjdk

--- CoinCalculator/CoinCalculator/BFCoinManager/Model/Market.swift
+++ CoinCalculator/CoinCalculator/BFCoinManager/Model/Market.swift
@@ -13,4 +13,15 @@ import Foundation
     { "product_code": "FX_BTC_JPY" },
     { "product_code": "ETH_BTC" },
     {
-    "product_code": "BTCJPY28APR20
+    "product_code": "BTCJPY28APR2017",
+    "alias": "BTCJPY_MAT1WK"
+    },
+    {
+    "product_code": "BTCJPY05MAY2017",
+    "alias": "BTCJPY_MAT2WK"
+    }]
+ */
+struct Market {
+    var productCode :String?
+    var alias :String?
+    
