851421f536ffa0bf4796ff5a794b7b63cbc3f2b3
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2021-02-16 21:14:46 +0000

    new binaries

--- CoinCalculator/CoinCalculator/Controller/CoinInfoView.swift
+++ CoinCalculator/CoinCalculator/Controller/CoinInfoView.swift
@@ -3,4 +3,17 @@
 //  CoinCalculator
 //
 //  Created by jeonsangjun on 2018/02/08.
-//  Copyright © 2018年 archive-asia. All rights reserved
+//  Copyright © 2018年 archive-asia. All rights reserved.
+//
+
+import UIKit
+
+class CoinInfoView: UIView {
+    
+    let fontSize: CGFloat = 17
+    let fontName = ".SFUIText"
+    
+    var ticker: Ticker? {
+        didSet {
+            guard let ticker = ticker else {
+                retu
