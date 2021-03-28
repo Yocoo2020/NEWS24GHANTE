cc9c3b43b95a70b4528a17f08d19eac985598593
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2021-03-28 22:51:39 +0000

    mm: compaction: clear PG_migrate_skip based on compaction and reclaim activity

--- CoinCalculator/CoinCalculator/Controller/MarketPriceCell.swift
+++ CoinCalculator/CoinCalculator/Controller/MarketPriceCell.swift
@@ -1,4 +1,15 @@
 //
 //  MarketPriceCell.swift
 //  CoinCalculator
-/
+//
+//  Created by jeonsangjun on 2018/02/05.
+//  Copyright © 2018年 archive-asia. All rights reserved.
+//
+
+import UIKit
+
+class MarketPriceCell: UITableViewCell {
+    
+    var askRate: Rate? {
+        didSet {
+            if let askRate = askRate 
