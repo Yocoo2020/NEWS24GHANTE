423e909f148f2f0e9ec6c9fbb4e41fe85b29f966
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2021-04-12 07:25:45 +0000

    Attempt to fix travis build

--- CoinCalculator/CoinCalculator/Extention/UIColor.swift
+++ CoinCalculator/CoinCalculator/Extention/UIColor.swift
@@ -2,4 +2,12 @@
 //  UIColor.swift
 //  CoinCalculator
 //
-//  Created by jeonsangjun on 2018
+//  Created by jeonsangjun on 2018/02/23.
+//  Copyright © 2018年 archive-asia. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func rgb(r: Int, g: Int, b: Int, alpha: CGFloat) -> UIColor{
+        return UIColor(red: CGFloat(r) /
