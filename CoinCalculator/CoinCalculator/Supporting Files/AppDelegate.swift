ed18566b994583171b401035af93047de4edaad6
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2022-09-04 16:51:40 +0000

    updated TODO file

--- CoinCalculator/CoinCalculator/Supporting Files/AppDelegate.swift
+++ CoinCalculator/CoinCalculator/Supporting Files/AppDelegate.swift
@@ -1,2 +1,13 @@
 //
 //  AppDelegate.swift
+//  CoinCalculator
+//
+//  Created by jeonsangjun on 2018/01/31.
+//  Copyright © 2018年 archive-asia. All rights reserved.
+//
+
+import UIKit
+import PubNub
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate 
