4fcd25f976074fd629b30afa2959b5272e1057fc
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2023-03-25 12:07:14 +0000

    updated color

--- CoinCalculator/CoinCalculatorUITests/CoinCalculatorUITests.swift
+++ CoinCalculator/CoinCalculatorUITests/CoinCalculatorUITests.swift
@@ -17,4 +17,7 @@ class CoinCalculatorUITests: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must la
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state -
