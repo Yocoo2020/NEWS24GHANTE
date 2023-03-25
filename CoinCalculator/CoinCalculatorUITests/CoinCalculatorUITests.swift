d8632ce98c5c870ef84eac23b2af3dd98ae24d6f
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2023-03-25 06:44:05 +0000

    Bump 1.1.0

--- CoinCalculator/CoinCalculatorUITests/CoinCalculatorUITests.swift
+++ CoinCalculator/CoinCalculatorUITests/CoinCalculatorUITests.swift
@@ -2,4 +2,15 @@
 //  CoinCalculatorUITests.swift
 //  CoinCalculatorUITests
 //
-//  Created by jeonsangju
+//  Created by jeonsangjun on 2018/01/31.
+//  Copyright © 2018年 archive-asia. All rights reserved.
+//
+
+import XCTest
+
+class CoinCalculatorUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code h
