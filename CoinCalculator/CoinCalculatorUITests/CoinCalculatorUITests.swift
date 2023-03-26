f9e41b70dccae6f56062aaf240f1e3267dbe2cbe
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2023-03-26 13:40:31 +0000

    update projects

--- CoinCalculator/CoinCalculatorUITests/CoinCalculatorUITests.swift
+++ CoinCalculator/CoinCalculatorUITests/CoinCalculatorUITests.swift
@@ -30,4 +30,7 @@ class CoinCalculatorUITests: XCTestCase {
     
     func testExample() {
         // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
