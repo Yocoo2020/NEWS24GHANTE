52158088541999e5fab44ff587bd58ac71452eb3
Yocoo2020 <88492616+Yocoo2020@users.noreply.github.com>
2023-03-26 08:35:07 +0000

    drm/i915: hold forcewake around ring hw init

--- CoinCalculator/CoinCalculatorUITests/CoinCalculatorUITests.swift
+++ CoinCalculator/CoinCalculatorUITests/CoinCalculatorUITests.swift
@@ -24,4 +24,10 @@ class CoinCalculatorUITests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after th
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce
